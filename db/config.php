0876f5b902903071f37425d27cd803837d06f86e
Thorsten Schüller <thorsten-nl@schueller.me>
2014-01-25 20:52:17 +0100

    removed config.php, added config-default.php instead

--- db/config.php
+++ db/config.php
@@ -1,57 +1,33 @@
 <?php
-
-# Configuration goes here
 $config = array();
-
-# Database type: sqlite or mysql
-$config['db'] = '';
-
-# Specify these settings if you selected above to use Mysql
-$config['mysql.host'] = "localhost";
-$config['mysql.db'] = "mytinytodo";
-$config['mysql.user'] = "user";
-$config['mysql.password'] = ""; 
-
-# Tables prefix
-$config['prefix'] = "mtt_";
-
-# These two parameters are used when mytinytodo index.php called not from installation directory
-# 'url' - URL where index.php is called from (ex.: http://site.com/todo.php)
-# 'mtt_url' - directory URL where mytinytodo is installed (with trailing slash) (ex.: http://site.com/lib/mytinytodo/)
+$config['db'] = 'mysql';
+$config['mysql.host'] = 'localhost';
+$config['mysql.db'] = 'schueller';
+$config['mysql.user'] = 'root';
+$config['mysql.password'] = 'eiw2EiC5aich';
+$config['postgres.host'] = 'localhost';
+$config['postgres.db'] = 'yourtinytodo';
+$config['postgres.user'] = 'user';
+$config['postgres.password'] = '';
+$config['prefix'] = 'ytt_';
 $config['url'] = '';
-$config['mtt_url'] = ''; 
-
-# Language pack
-$config['lang'] = "en";
-
-# Specify password here to protect your tasks from modification,
-#  or leave empty that everyone could read/write todolist
-$config['password'] = "";
-
-# To disable smart syntax uncomment the line below
-#$config['smartsyntax'] = 0;
-
-# Default Time zone
+$config['ytt_url'] = '';
+$config['title'] = 'test todo';
+$config['lang'] = 'en';
+$config['password'] = '';
+$config['multiuser'] = 0;
+$config['smartsyntax'] = 1;
 $config['timezone'] = 'UTC';
-
-# To disable auto adding selected tag  comment out the line below or set value to 0
 $config['autotag'] = 1;
-
-# duedate calendar format: 1 => y-m-d (default), 2 => m/d/y, 3 => d.m.y
 $config['duedateformat'] = 1;
-
-# First day of week: 0-Sunday, 1-Monday, 2-Tuesday, .. 6-Saturday
 $config['firstdayofweek'] = 1;
-
-# select session handling mechanism: files or default (php default)
-$config['session'] = 'files';
-
-# Date/time formats
+$config['session'] = 'default';
 $config['clock'] = 24;
 $config['dateformat'] = 'j M Y';
+$config['dateformat2'] = 'd/m/y';
 $config['dateformatshort'] = 'j M';
-
-# Show task date in list
+$config['template'] = 'default';
 $config['showdate'] = 0;
-
+$config['auth_bypass'] = 'none';
+$config['debugmode'] = 0;
 ?>
